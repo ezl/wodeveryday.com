80c0d1144ebfe814ddecdf9a4d5d75ef7194bc77
cb0806151 <cb0806151@otc.edu>
2020-05-14 19:23:56 -0500

    test out alternate gtm implementation

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -19,9 +19,22 @@ export default {
       },
     ],
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    __dangerouslyDisableSanitizers: ["script"],
     script: [
       {
         src: `https://maps.googleapis.com/maps/api/js?key=${process.env.GCP_API_KEY}&libraries=places`,
+      },
+      {
+        innerHTML: `(function (w, d, s, l, i) {
+          w[l] = w[l] || []
+          w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" })
+          var f = d.getElementsByTagName(s)[0],
+            j = d.createElement(s),
+            dl = l != "dataLayer" ? "&l=" + l : ""
+          j.async = true
+          j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl
+          f.parentNode.insertBefore(j, f)
+        })(window, document, "script", "dataLayer", "${process.env.GTM_ID}")`,
       },
     ],
   },
@@ -40,16 +53,7 @@ export default {
   /*
    ** Nuxt.js dev-modules
    */
-  buildModules: [
-    "@nuxt/typescript-build",
-    "@nuxtjs/vuetify",
-    "@nuxtjs/dotenv",
-    "@nuxtjs/gtm",
-  ],
-  gtm: {
-    id: process.env.GTM_ID,
-    pageTracking: true,
-  },
+  buildModules: ["@nuxt/typescript-build", "@nuxtjs/vuetify", "@nuxtjs/dotenv"],
   /*
    ** Nuxt.js modules
    */
