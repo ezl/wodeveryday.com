d765354b93984a6a144ae6e6d5ae7fe1dfc62d89
cb0806151 <cb0806151@otc.edu>
2020-05-27 16:44:44 -0500

    create computed property for logic and create constant for default price

--- front/components/PriceCard.vue
+++ front/components/PriceCard.vue
@@ -4,16 +4,13 @@
       <h3>Price Info</h3>
     </v-card-text>
     <v-divider />
-    <v-card-text v-if="dropInPrice !== '??.??'">
+    <v-card-text v-if="dropInPrice !== $store.state.constants.DEFAULT_PRICE">
       Drop-in: ${{ dropInPrice }}
     </v-card-text>
-    <v-card-text v-if="monthlyPrice !== '??.??'">
+    <v-card-text v-if="monthlyPrice !== $store.state.constants.DEFAULT_PRICE">
       Monthly: ${{ monthlyPrice }}
     </v-card-text>
-    <v-card-text
-      v-if="dropInPrice === '??.??' && monthlyPrice === '??.??'"
-      class="ma-2 red--text"
-    >
+    <v-card-text v-if="pricingUnavailableMessageVisible" class="ma-2 red--text">
       We don't have pricing info for this gym yet, please visit
       <a :href="gymWebsite" target="_blank">their website</a>
       for more information
@@ -39,6 +36,14 @@ export default {
       type: String,
       required: false,
       default: "#",
+    },
+  },
+  computed: {
+    pricingUnavailableMessageVisible: function () {
+      return (
+        this.dropInPrice === this.$store.state.constants.DEFAULT_PRICE &&
+        this.monthlyPrice === this.$store.state.constants.DEFAULT_PRICE
+      )
     },
   },
 }
