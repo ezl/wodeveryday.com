791ef6b514002f090b652e0a9b65009fd0588ddb
Igor k9 <k9.developer@gmail.com>
2020-09-02 13:18:55 +0300

    ticket 131: set landing page to use `/` path

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,7 +1,59 @@
+<template>
+  <geography-search-page
+    :item-list="$store.state.continents"
+    :select-item="selectContinent"
+    :select-subitem="selectCountry"
+  />
+</template>
+
 <script>
+import GeographySearchPage from "~/components/navigation/GeographySearchPage.vue"
+import apiLibrary from "~/store/apiLibrary.js"
+import reusableFunctionsLibrary from "~/store/reusableFunctionsLibrary.js"
+
 export default {
-  fetch({ redirect }) {
-    redirect("/find")
+  components: {
+    GeographySearchPage,
+  },
+  async asyncData({ store, route }) {
+    const url = `${process.env.BACKEND_URL}/gyms/continents`
+    await apiLibrary.retrieveContinents(url, store)
+
+    let metaTags = reusableFunctionsLibrary.generateMetaTags(
+      store,
+      store.state.constants.HOME_PAGE_TITLE,
+      store.state.constants.DEFAULT_META_DESCRIPTION,
+      store.state.constants.DEFAULT_GYM_THUMBNAIL,
+      route.fullPath
+    )
+    return {
+      metaTags: metaTags,
+    }
+  },
+  data() {
+    return {
+      metaTags: [],
+    }
+  },
+  methods: {
+    selectContinent(continentName) {
+      reusableFunctionsLibrary.pushCleanedRoute(
+        this.$router,
+        `find/${continentName}/`
+      )
+    },
+    selectCountry(continentName, countryName) {
+      reusableFunctionsLibrary.pushCleanedRoute(
+        this.$router,
+        `find/${continentName}/${countryName}/`
+      )
+    },
+  },
+  head() {
+    return {
+      title: this.$store.state.constants.HOME_PAGE_TITLE,
+      meta: this.metaTags,
+    }
   },
 }
 </script>
