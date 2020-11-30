c081134bb07b35564e60370796914a6d0d9c5fc7
Aldarund <aldarund@gmail.com>
2020-11-30 23:26:23 +0300

    fix: one router

--- back/app/urls.py
+++ back/app/urls.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from core.gym.urls import gym_router, gym_details_router, gym_leaderboard_router
+from core.gym.urls import router
 
 
 urlpatterns = [
-    path('', include(gym_router.urls)),
-    path('', include(gym_leaderboard_router.urls)),
-    path('', include(gym_details_router.urls)),
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
